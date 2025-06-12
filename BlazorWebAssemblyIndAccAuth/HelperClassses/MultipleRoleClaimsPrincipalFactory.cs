06a364106cff2378c0184c8ee12ef3f6f9d68a00
Amandeep Singh <amandeepsingh@Amandeeps-MacBook-Pro.local>
2025-06-12 16:27:25 -0700

    Login, Logout and Register new user on Keycloak login screen, and update Realm Json to make auth work, send roles in the token and Blazor to consume token roles

---
+++ BlazorWebAssemblyIndAccAuth/HelperClassses/MultipleRoleClaimsPrincipalFactory.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Security.Claims;
+using System.Text.Json;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication.Internal;
+
+namespace BlazorWebAssemblyIndAccAuth.HelperClassses;
+
+public class MultipleRoleClaimsPrincipalFactory<TAccount> : AccountClaimsPrincipalFactory<TAccount>
+    where TAccount : RemoteUserAccount
+{
+    public MultipleRoleClaimsPrincipalFactory(IAccessTokenProviderAccessor accessor)
+        : base(accessor)
+    {
+    }
+    public async override ValueTask<ClaimsPrincipal> CreateUserAsync(TAccount account, 
+        RemoteAuthenticationUserOptions options)
+    {
+        var user = await base.CreateUserAsync(account, options);
+        var claimsIdentity = user.Identity as ClaimsIdentity;
+        if (account != null && claimsIdentity != null)
+        {
+            MapArrayClaimsToMultipleSeparateClaims(account, claimsIdentity);
+        }
+        return user;
+    }
+    private static void MapArrayClaimsToMultipleSeparateClaims(TAccount account, ClaimsIdentity claimsIdentity)
+    {
+        foreach (var prop in account.AdditionalProperties)
+        {
+            var key = prop.Key;
+            var value = prop.Value;
+            if (value != null &&
+                value is JsonElement element && element.ValueKind == JsonValueKind.Array)
+            {
+                claimsIdentity.RemoveClaim(claimsIdentity.FindFirst(prop.Key));
+                var claims = element.EnumerateArray()
+                    .Select(x => new Claim(prop.Key, x.ToString()));
+                claimsIdentity.AddClaims(claims);
+            }
+        }
+    }
+}
--- BlazorWebAssemblyIndAccAuth/Program.cs
+++ BlazorWebAssemblyIndAccAuth/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorWebAssemblyIndAccAuth;
+using BlazorWebAssemblyIndAccAuth.HelperClassses;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -18,8 +21,9 @@ builder.Services.AddOidcAuthentication(options =>
     options.ProviderOptions.Authority = "http://localhost:8080/realms/CustomRealmName";
     options.ProviderOptions.ClientId = "frontend";
     options.ProviderOptions.ResponseType = "code"; // Authorization Code flow
+    options.UserOptions.RoleClaim = "role"; // request the role claim from Keycloak
     // Optional: If using custom scope for audience, request it
-    // options.ProviderOptions.DefaultScopes.Add("myapi_scope");
-});
+    // options.ProviderOptions.DefaultScopes.Add("role_list");
+}).AddAccountClaimsPrincipalFactory<MultipleRoleClaimsPrincipalFactory<RemoteUserAccount>>();
 
 await builder.Build().RunAsync();
